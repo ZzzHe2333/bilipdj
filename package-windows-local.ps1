10a977787f929954d6083e8d0cd2dccb2ca1f408
ZzzHe <x121144@163.com>
2026-09-08 17:18:08 +0800

    chore: remove obsolete root Windows packaging wrapper

--- package-windows-local.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [switch]$InstallDependencies,
-    [string]$PythonExe = "python"
-)
-
-$ErrorActionPreference = "Stop"
-& "$PSScriptRoot\apps\windows\package.ps1" -InstallDependencies:$InstallDependencies -PythonExe $PythonExe
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
